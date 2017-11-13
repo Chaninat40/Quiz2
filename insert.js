b725b8d44879bee06dea6db0a549a365f752ef33
Chaninat40 <milk11288@msn.com>
2017-11-13 11:41:20 +0700

    update2
    update2

--- insert.js
+++ insert.js
@@ -2,7 +2,7 @@ var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/quiz2";
 
 MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
+ if (err) throw err;
   var myobj = [
     {
         "id": 1,"first_name": "Peter","address": "Highway 71",
@@ -43,11 +43,10 @@ MongoClient.connect(url, function(err, db) {
     {
         "id": 10,"first_name": "John","address": "Yellow Garden 2",
         "expired": false,"role": ["admin","user"]
-    } 
+    }
         
   ];
-
-  db.collection("users").insertMany(quiz2, function(err, res) {
+  db.collection("users").insertMany(myobj, function(err, res) {
     if (err) throw err;
     console.log("1 document inserted");
     db.close();
